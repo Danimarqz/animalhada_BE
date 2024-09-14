5717098d79fc4261c634c76c56a70ffa4015fcdd
Danimarqz <daniel.marquez@ciclosmontecastelo.com>
2024-09-14 12:45:31 +0200

    fix client repo not returning correctly

--- src/repository/clientRepo.js
+++ src/repository/clientRepo.js
@@ -9,16 +9,16 @@ export const GetAll = async (db, from=0, to=20) => {
 
 export const GetById = async (db, id) => {
 	const { data, error, status } = await createSupabaseClient(db).from('client')
-	.select().eq('id', id);
+	.select('*').eq('id', id);
 	if (error) throw error;
-	return data, status;
+	return { data, status };
 }
 
 export const Create = async(db, newClient) => {
 	const { data, error, status } = await createSupabaseClient(db).from('client')
 	.insert([newClient]);
 	if (error) throw error;
-	return data, status;
+	return { data, status };
 }
 
 export const Update = async(db, id, client) => {
