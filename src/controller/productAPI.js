015effe648a7a2dfc0dcf19c2f41aa9003223fa3
Danimarqz <daniel.marquez@ciclosmontecastelo.com>
2024-09-14 11:53:15 +0200

    product api logic separated. Pagination, ordering and category filter added

--- src/controller/productAPI.js
+++ src/controller/productAPI.js
@@ -1,50 +1,76 @@
-// src/templates/basic/productAPI.js
-import { createSupabaseClient } from './supabaseClient';
+import { Create, GetAll, GetByCategory, GetById, Update, Delete } from "../repository/productRepo";
+import { errorMsg, getPaginationLimits } from "../utils/utils";
 
-const productAPI = (app, env) => {
-	const supabase = createSupabaseClient(env);
-	app.get('/product', async (c) => {
-		const { data, error } = await supabase.from('product').select('*');
+const productAPI = (app, db) => {
+	//all products paginated with ordering by price
+	app.get('/product/:page?/:order?', async (c) => {
+		const pagination = getPaginationLimits(c.req.param('page'));
+		const order = c.req.param('order') || null;
+		try {
+			const { data, status } = pagination
+			? await GetAll(db, pagination.from, pagination.to, order)
+			: await GetAll(db, order);
 
-		if (error) {
-		return c.json({ error: error.message }, 500);
+			return c.json(data, status)
+		} catch (error) {
+			return errorMsg(c, error);
 		}
-
-		return c.json(data);
 	});
+	//all products filtered by category, with pagination and ordering by price
+	app.get('/product/:category/:page?/:order?', async (c) => {
+		const pagination = getPaginationLimits(c.req.param('page'));
+		const category = c.req.param('category');
+		const order = c.req.param('order') || null;
+		try {
+			const { data, status } = pagination
+			? await GetByCategory(db, category, pagination.from, pagination.to, order)
+			: await GetByCategory(db, category, order);
 
+			return c.json(data, status);
+		} catch (error) {
+			return errorMsg(c, error);
+		}
+	});
+	//1 product by id
+	app.get('/product/:id', async (c) => {
+		const id = c.req.param('id');
+		try {
+			const { data, status } = await GetById(db, id);
+			return c.json(data, status);
+		} catch (error) {
+			return errorMsg(c, error);
+		}
+	})
+	// new product
 	app.post('/product', async (c) => {
-		const newproduct = await c.req.json();
-		const { data, error } = await supabase.from('product').insert([newproduct]);
-
-		if (error) {
-		return c.json({ error: error.message }, 500);
+		const newProduct = await c.req.json();
+		try {
+			const { data, status } = await Create(db, newProduct);
+			return c.json(data, status);
+		} catch(error) {
+			return errorMsg(c, error);
 		}
-
-		return c.json(data);
 	});
-
+	// update product
 	app.put('/product/:id', async (c) => {
 		const id = c.req.param('id');
 		const updates = await c.req.json();
-		const { data, error } = await supabase.from('product').update(updates).eq('id', id);
-
-		if (error) {
-		return c.json({ error: error.message }, 500);
+		try {
+			const { data, status } = await Update(db, id, updates);
+			return c.json(data, status);
+		} catch(error) {
+			return errorMsg(c, error);
 		}
-
-		return c.json(data);
 	});
-
+	// delete product
 	app.delete('/product/:id', async (c) => {
 		const id = c.req.param('id');
-		const { data, error } = await supabase.from('product').delete().eq('id', id);
-
-		if (error) {
-		return c.json({ error: error.message }, 500);
+		try {
+			const { status } = await Delete(db, id);
+			return c.json(status);
+		} catch(error) {
+			return errorMsg(c, error);
 		}
-
-		return c.json(data);
 	});
 };
 
--- src/repository/productRepo.js
+++ src/repository/productRepo.js
@@ -1,23 +1,25 @@
 import { createSupabaseClient } from "../db"
 
 
-export const GetAll = async (db, from=0, to=20) => {
-	const { data, error, status } = await createSupabaseClient(db).from('product')
-	.select('*').range(from, to);
+export const GetAll = async (db, from=0, to=20, order=true) => {
+	const { data, error, status } = order == null
+	? await createSupabaseClient(db).from('product').select('*').range(from, to)
+	: await createSupabaseClient(db).from('product').select('*').order('price', {ascending: order}).range(from, to);
 	if (error) throw error;
 	return { data, status };
 }
+
 
 export const GetById = async (db, id) => {
-	const { data, error, status } = await createSupabaseClient(db).from('product')
-	.select().eq('id', id);
+	const { data, error, status } = await createSupabaseClient(db).from('product').select().eq('id', id);
 	if (error) throw error;
 	return { data, status };
 }
 
-export const GetByCategory = async (db, category, from=0, to=20) => {
-	const { data, error, status } = await createSupabaseClient(db).from('product')
-	.select().eq('category', category).range(from, to);
+export const GetByCategory = async (db, category, from=0, to=20, order=true) => {
+	const { data, error, status } = order == null
+	? await createSupabaseClient(db).from('product').select('*').eq('category', category).range(from, to)
+	: await createSupabaseClient(db).from('product').select('*').eq('category', category).order('price', { ascending: order }).range(from, to);
 	if (error) throw error;
 	return { data, status };
 }
