d8cf3d7b6295704ec1c29ed8431e7fc4fb0ee2b0
Danimarqz <daniel.marquez@ciclosmontecastelo.com>
2024-09-16 23:26:03 +0200

    fix env type

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { ExecutionContext, Hono } from 'hono';
 import clientAPI from './controller/clientAPI';
 import productAPI from './controller/productAPI';
+import { Env } from './types/env';
 
 export default {
-  async fetch(request: Request, env: any, context: ExecutionContext): Promise<Response> {
+  async fetch(request: Request, env: Env, context: ExecutionContext): Promise<Response> {
     const app = new Hono();
     const api = app.basePath('/api');
 
-    // Pasar el tipo de `env` al API de clientes y productos
     clientAPI(api, env);
     productAPI(api, env);
 
